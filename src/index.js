50e663d0258e8321d36b88b99d8f695686e3f87c
ZawadiSumba66 <rosesumba98@gmail.com>
2021-06-08 13:04:53 +0300

    Fix code review issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store';
+import { actionCreate } from './actions/index';
+
+store.dispatch(actionCreate({ id: Math.random(), title: 'I am Malala', category: 'Action' }));
+store.dispatch(actionCreate({ id: Math.random(), title: 'The Survivors', category: 'Horror' }));
+store.dispatch(actionCreate({ id: Math.random(), title: 'Ghost Rider', category: 'History' }));
+store.dispatch(actionCreate({ id: Math.random(), title: 'Ghost Rider', category: 'Biography' }));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,12 +1,7 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
 const initialState = {
-  books: [
-    { id: Math.random(), title: 'I am Malala', category: 'Action' },
-    { id: Math.random(), title: 'The Survivors', category: 'Horror' },
-    { id: Math.random(), title: 'Ghost Rider', category: 'History' },
-    { id: Math.random(), title: 'Ghost Rider', category: 'Biography' },
-  ],
+  books: [],
 };
 
 export default function appReducer(state = initialState, action) {
