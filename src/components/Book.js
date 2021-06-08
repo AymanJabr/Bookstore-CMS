540893cb8ffcacff8b94768d4a46fc8673e2fb0a
ayman <aymanjaber2012@gmail.com>
2021-06-08 18:42:46 +0300

    Fix code review issues

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,30 +1,25 @@
 import PropTypes from 'prop-types';
-import { actionRemove } from '../actions';
-import store from '../store';
 
-const Book = ({ id, title, category }) => {
-  const removeBook = () => {
-    store.dispatch(actionRemove({ id }));
-  };
-
-  return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td>
-        {' '}
-        <button type="button" onClick={removeBook}>Remove Book</button>
-        {' '}
-      </td>
-    </tr>
-  );
-};
+const Book = ({
+  id, title, category, removeBook,
+}) => (
+  <tr>
+    <td>{id}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+    <td>
+      {' '}
+      <button type="button" onClick={() => removeBook(id)}>Remove Book</button>
+      {' '}
+    </td>
+  </tr>
+);
 
 Book.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default Book;
